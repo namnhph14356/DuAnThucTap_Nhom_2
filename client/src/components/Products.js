81c89d1f8365c9372b4ae90341d04abafb0b5617
pvq2k2 <vanquyet2392002e@gmail.com>
2022-10-09 09:48:38 +0700

    XTTN2-5 [Client] Adding Rating bar

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-const Products = ({product}) => {
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "react-rating";
+const Products = ({ product }) => {
   return (
-        <div className='col-md-3 card m-5 p-2' >
-            <Link to={`/product/${product.id}`}>
-            <div key={product.id}>
-            <h1>{product.name}</h1>
-                            <img src={product.image} alt='' className='img-fluid' />
-                            <h1>Rating: {product.rating}</h1>
-                            <h1>Price: {product.price}</h1>
-            </div>
-            </Link>        
+    <div className="col-md-3 card m-5 p-2 text-start">
+      <Link to={`/product/${product.id}`}>
+        <div key={product.id}>
+          <img src={product.image} alt="" className="img-fluid" />
+          <h1>{product.name}</h1>
+          <Rating
+            style={{ color: "orange" }}
+            initialRating={product.rating}
+            emptySymbol="fa-regular fa-star"
+            fullSymbol="fa-solid fa-star"
+            readonly={true}
+          />
+          <h1>Price: {product.price}</h1>
         </div>
-    )
-}
+      </Link>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
